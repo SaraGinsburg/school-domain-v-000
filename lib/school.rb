5349423eda11b3ae78e2e387e3ed882577616e80
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-23 02:58:57 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
 class School
   attr_accessor :roster, :name
-  
+
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def add_student(grade, name)
-    
+  def add_student(name, grade)
+
     @roster.has_key? "grade" ? roster[:grade] << name : roster[:grade] = [name]
   end
 end
